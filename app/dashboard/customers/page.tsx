28ffcf20bb5efe06e110087536bfa6237c561ae4
Half_Barba <barbara.sandrolini@gmail.com>
2024-01-22 11:00:18 +0100

    Metadata and Customer Page added

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,33 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <Suspense fallback={<InvoicesTableSkeleton />}>
+        <CustomersTable customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
